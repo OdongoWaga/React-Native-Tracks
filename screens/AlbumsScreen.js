c0672a7bae62a8db28c6b4281d96f450533a46f2
Waga <wagaodongo@gmail.com>
2019-07-10 22:50:20 +0300

    react native elements

---
+++ screens/AlbumsScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import {Card, Text, Button} from 'react-native-elements';
+
+
+export default function AlbumsScreen() {
+  return (
+    <ScrollView style={styles.container}>
+     <Card
+     title='Hello World'>
+         <Text style={{marginBottom: 10}}>
+             The idea with React Native Elements is to be great
+         </Text>
+         <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    backgroundColor='#03A9F4'
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+     </Card>
+
+    </ScrollView>
+  );
+}
+
+AlbumsScreen.navigationOptions = {
+  title: 'Links',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Button,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  render () {
+  return (
+    <View style= {styles.container} >
+      <Button title='Navigate to Albums' onPress={()=> this.props.navigation.navigate('Albums')} />
+    </View>
+
+
+        
+  );
+  }
+}
+
+HomeScreen.navigationOptions = {
+  title: 'Home',
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
