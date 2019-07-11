c3c23e1fe18a8ec97f8f211b89a30f9918bb8f6a
Waga <wagaodongo@gmail.com>
2019-07-11 11:24:16 +0300

    cardName

--- screens/AlbumsScreen.js
+++ screens/AlbumsScreen.js
@@ -4,11 +4,27 @@ import {Card, Text, Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
-export default function AlbumsScreen() {
+export default class AlbumsScreen extends React.Component {
+  state= {
+    cardName: 'Some Album',
+
+  }
+
+  cardNumber = 0;
+
+  changeCardName =()=> {
+    this.setState({
+      cardName: `I am a new CardName ${this.cardNumber}`
+    })
+    this.cardNumber++
+  }
+
+  render(){
+    const {cardName}= this.state;
   return (
     <ScrollView style={styles.container}>
      <Card
-     title='Hello World'>
+     title={cardName}>
          <Text style={{marginBottom: 10}}>
              The idea with React Native Elements is to be great
          </Text>
@@ -18,9 +34,16 @@ export default function AlbumsScreen() {
     buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
     title='VIEW NOW' />
      </Card>
+     <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    backgroundColor='#03A9F4'
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='Change Card Name' 
+    onPress={()=> {this.changeCardName()}}/>
 
     </ScrollView>
   );
+}
 }
 
 AlbumsScreen.navigationOptions = {
