cb5a79ae01c7b88328bd3c7f8982419380eaf8ed
Waga <wagaodongo@gmail.com>
2019-07-11 16:06:38 +0300

    API calls

--- actions/index.js
+++ actions/index.js
@@ -1,1 +1,15 @@
 import axios from 'axios';
+
+ const API_KEY='4cbab99e0emsh7d261d6e65293a6p1b6b13jsnfa2c70b020ae'
+
+ const axiosInstance = axios.create({
+     baseURL:'https://deezerdevs-deezer.p.rapidapi.com/',
+     timeout: 2000,
+     headers: {'X-RapidAPI-Key': API_KEY}
+ });
+
+ export const searchTracks =singer => {
+     return axiosInstance.get(`search?q=${singer}`). then (
+        response => response.data.data.map((item)=> item.album)
+     );
+ }
--- screens/AlbumsScreen.js
+++ screens/AlbumsScreen.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import {Card, Text, Button} from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+//import {Card, Text, Button} from 'react-native-elements';
+//import Icon from 'react-native-vector-icons/FontAwesome';
 import CardList from '../components/CardList';
+import * as actions from '../actions';
 
 
 export default class AlbumsScreen extends React.Component {
-  state= {
-    albums: [
-      {
-        title: 'Meteora',
-        image: 'https://i.pinimg.com/originals/c1/db/04/c1db04e854d022ea38b504e7f2a38025.jpg'
-      },
-      {
-        title: 'Meteora',
-        image: 'https://i.pinimg.com/originals/c1/db/04/c1db04e854d022ea38b504e7f2a38025.jpg'
-      },
-      {
-        title: 'Meteora',
-        image: 'https://i.pinimg.com/originals/c1/db/04/c1db04e854d022ea38b504e7f2a38025.jpg'
-      },
-      {
-        title: 'Meteora',
-        image: 'https://i.pinimg.com/originals/c1/db/04/c1db04e854d022ea38b504e7f2a38025.jpg'
-      },
-    ]
+  constructor() {
+    super();
+  this.state= {
+    albums: []
+  };
 
+  actions.searchTracks('eminem').then(albums => this.setState({albums}));
   }
+  
 
   
 
@@ -35,13 +24,10 @@ export default class AlbumsScreen extends React.Component {
     const {albums} = this.state;
     
   return (
-    <ScrollView style={styles.container}>
-      
-    <CardList data={albums} imageKey={'image'} titleKey={'title'}
+    <ScrollView style={styles.container}>     
+    <CardList data={albums} imageKey={'cover_big'} titleKey={'title'}
     buttonText="See the detail">
-
     </CardList>
-
     </ScrollView>
   );
 }
