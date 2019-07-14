8766aff3726b39345c9cb0f763e72f1b43d457cd
Waga <wagaodongo@gmail.com>
2019-07-14 10:35:48 +0300

    Homescreen page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,6 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Text,
   TouchableOpacity,
   Button,
   View,
@@ -14,12 +13,51 @@ import { List, ListItem, Text, Icon, Card } from 'react-native-elements';
 
 import { MonoText } from '../components/StyledText';
 
+const menuList = [
+  {
+    title: 'Search Albums',
+    subtitle: 'Find your favorite music',
+    icon: 'music',
+    navigateTo: 'Albums'
+  },
+  {
+    title: 'Favorite Collections',
+    subtitle: 'Access your favorite albums',
+    icon: 'heart',
+    navigateTo: 'Favorite'
+  },
+  {
+    title: 'Settings',
+    subtitle: 'Customize your app',
+    icon: 'cog',
+    navigateTo: '#'
+  }
+]
+
 export default class HomeScreen extends React.Component {
   render () {
   return (
-    <View style= {styles.container} >
-      <Button title='Navigate to Albums' onPress={()=> this.props.navigation.navigate('Albums')} />
-    </View>
+    <ScrollView style={styles.container}>
+     <View>
+  {
+    menuList.map((item, index) => (
+              <Card key={index}
+                      title={item.title}>
+                  <View style={styles.cardView}>
+                    <Text style={{marginBottom: 10}}> {item.subtitle} </Text>
+                    <Icon
+                      raised
+                      name={item.icon}
+                      type='font-awesome'
+                      color='#f50'
+                      size={30}
+                      onPress={() => {this.props.navigation.navigate(item.navigateTo)}}/>
+                  </View>
+                </Card>
+    ))
+  }
+</View>
+    </ScrollView>
 
 
         
